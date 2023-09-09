83a976d0b82900f65237cd1d023884311624ccda
Anna Kors <ankors320@gmail.com>
2023-09-10 00:35:54 +0300

    Added task description

--- HW2/Task1/Main.java
+++ HW2/Task1/Main.java
@@ -4,6 +4,13 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+
+    /*
+    Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float),
+    и возвращает введенное значение. Ввод текста вместо числа не должно приводить к падению приложения,
+    вместо этого, необходимо повторно запросить у пользователя ввод данных.
+    */
+
         public static void main(String[] args) {
             boolean flag = false;
             float num = 0;
