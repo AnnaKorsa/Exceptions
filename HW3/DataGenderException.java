8f9b0e5210aca6df13973611f095c882575732ee
Anna Kors <ankors320@gmail.com>
2023-09-16 00:17:44 +0300

    Done

---
+++ HW3/DataGenderException.java
@@ -0,0 +1,7 @@
+package HW3;
+
+public class DataGenderException extends RuntimeException{
+    public DataGenderException(String message) {
+        super(message);
+    }
+}
---
+++ HW3/LengthInputException.java
@@ -0,0 +1,7 @@
+package HW3;
+
+public class LengthInputException extends RuntimeException{
+    public LengthInputException(String message) {
+        super(message);
+    }
+}
